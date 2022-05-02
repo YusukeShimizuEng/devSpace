84e16e1577a5ae3d238126d96e7af2210882484e
YusukeShimizuEng <ajmdwtpgajdm@gmail.com>
2022-05-02 21:08:40 +0900

    first commit

---
+++ voiceRecognition/recognition.js
@@ -0,0 +1,30 @@
+const speech = new webkitSpeechRecognition();
+speech.lang = 'ja-JP';
+
+const btn = document.getElementById('btn');
+const content = document.getElementById('content');
+
+btn.addEventListener('click',function(){
+    speech.start();
+});
+
+/*
+speech.addEventListener('result',function(e){
+    console.log(e);
+
+    const text = e.results[0][0].transcript;
+    content.innerText = text;
+});
+*/
+
+speech.onresult = function(e){
+    speech.stop();
+    if(e.results[0].isFinal){
+        var autotext = e.results[0][0].transcript;
+        content.innerHTML += '<div>' + autotext +'</div>'
+    }
+};
+
+speech.onend = () => {
+    speech.start();
+};
